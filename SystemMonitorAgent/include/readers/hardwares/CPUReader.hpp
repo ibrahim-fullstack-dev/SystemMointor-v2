0eec6cab07eb07b32c6bc4a476f6681ac4a56b4a
ibrahim <ibrahim.fullstack.dev@gmail.com>
2026-05-25 08:39:24 +0300

    fix: Move header code from CPUReader.cpp into CPUReader.hpp

---
+++ SystemMonitorAgent/include/readers/hardwares/CPUReader.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <windows.h>
+#include <cstdint>
+#include <array>
+#include "models/hardwares/CPUDTO.hpp"
+
+namespace System {
+    namespace DAL {
+        namespace Hardware {
+
+            namespace CPU {
+
+                void FetchCoreMetrics(
+                    FILETIME& inOutPrevIdle,
+                    FILETIME& inOutPrevKernel,
+                    FILETIME& inOutPrevUser,
+                    Model::Hardware::CPU::stCPUDTO& outCpuDTO);
+            }
+
+            namespace Cores {
+
+                void FetchPerCoreMetrics(
+                    std::array<FILETIME, Model::Hardware::CPU::MAX_SUPPORTED_CORES>& inOutPrevCoresIdle,
+                    std::array<FILETIME, Model::Hardware::CPU::MAX_SUPPORTED_CORES>& inOutPrevCoresKernel,
+                    std::array<FILETIME, Model::Hardware::CPU::MAX_SUPPORTED_CORES>& inOutPrevCoresUser,
+                    Model::Hardware::CPU::stCoreDTO& outCoresDTO);
+
+            }
+
+            namespace Processes {
+
+                void FetchAll(Model::Hardware::CPU::stProcessDTO& outProcessDTO);
+            }
+
+            namespace Threads {
+
+
+                void FetchForProcess(uint32_t targetProcessId, Model::Hardware::CPU::stThreadDTO& outThreadDTO);
+            }
+
+        }
+    }
+}
