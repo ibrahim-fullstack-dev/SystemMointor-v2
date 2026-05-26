2c2750b3c9075ef597b8d72eb8772cf620a948e7
ibrahim <ibrahim.fullstack.dev@gmail.com>
2026-05-26 09:07:58 +0300

    feat: create enCorePerformanceType in inclue/enums/EnumProvider.hpp path

---
+++ SystemMonitorAgent/include/enums/EnumsProvider.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdint>
+
+namespace System {
+
+    namespace Enums {
+
+        enum class enCorePerformanceType : uint8_t {
+            Unified = 0,
+            Performance = 1,
+            Efficiency = 2
+        };
+
+    }
+}
